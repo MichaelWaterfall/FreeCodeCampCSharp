5d5ae52c46809957a8eae45d0163f4bd898d66a5
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-22 10:31:26 +0000

    if statements

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec1e60647b179534779bd9fd08c73591ffd4e73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d8a80715e53923f3231ae918e8328ba68d269d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
