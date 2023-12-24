59af22e25b33d5305ecc55d143f842530e4e161f
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-24 14:23:32 +0000

    CoinFlip challenge

---
+++ CsharpProjects/CoinFlip/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Random coin = new Random();
+int coinFlip = coin.Next(0, 2);
+
+Console.WriteLine(coinFlip == 0 ? "heads" : "tails");
+Console.WriteLine(coinFlip);
