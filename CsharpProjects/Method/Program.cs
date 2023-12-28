f743c4a30615a905ca6a90f136dd52f694bacbc9
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-28 13:06:10 +0000

    Debugger file created

--- CsharpProjects/Method/Program.cs
+++ CsharpProjects/Method/Program.cs
@@ -16,3 +16,33 @@ void tellFortune() {
 }
 
 tellFortune();
+
+string[,] corporate = 
+{
+    {"Robert", "Bavin",}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+void emailFunction(string name){
+    Console.WriteLine(name + externalDomain);
+}
+Console.WriteLine(corporate[0, 2]);
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    // display internal email addresses
+    emailFunction(corporate[i, i + 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    // display external email addresses
+}
