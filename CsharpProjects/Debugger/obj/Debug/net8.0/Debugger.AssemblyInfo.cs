a6fec62abb47053b7c0b819c156ccf0a6cc5454f
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-12-28 13:34:18 +0000

    Debugger challenge

--- CsharpProjects/Debugger/obj/Debug/net8.0/Debugger.AssemblyInfo.cs
+++ CsharpProjects/Debugger/obj/Debug/net8.0/Debugger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Debugger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f743c4a30615a905ca6a90f136dd52f694bacbc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Debugger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Debugger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
