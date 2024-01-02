a102d157d5dae27742aa626f0ca789c2a24be25a
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-01-02 11:43:50 +0000

    PettingZoo file created

--- CsharpProjects/CoinFlip/obj/Debug/net8.0/CoinFlip.AssemblyInfo.cs
+++ CsharpProjects/CoinFlip/obj/Debug/net8.0/CoinFlip.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoinFlip")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13970f8f7d564cc15661044615a9a616277d6383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoinFlip")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoinFlip")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
